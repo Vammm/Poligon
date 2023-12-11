f801f99ee858522f6a96cd1dfa8de6e757970590
Vammm <vam02415@gmail.com>
2023-12-11 17:54:32 +0300

    Install bootstrap and bootstrap-icons

--- resources/js/vue/test.vue
+++ resources/js/vue/test.vue
@@ -4,6 +4,15 @@
     </h1>
     <button type="button" class="btn">Base class</button>
     <button type="button" class="btn btn-primary">Primary</button>
+
+    <div class="card-body text-center">
+        <i class="bi bi-bag-heart-fill"></i>
+        <i class="bi bi-app"></i>
+        <i class="bi bi-arrow-right-square-fill"></i>
+        <i class="bi bi-bag-check-fill"></i>
+        <i class="bi bi-calendar-plus-fill"></i>
+    </div>
+
 </template>
 
 
